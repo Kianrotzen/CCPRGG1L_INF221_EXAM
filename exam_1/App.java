ee05788bfc86fbe97e396001009f63d8c35bebc5
Kianrotzen <111473023+Kianrotzen@users.noreply.github.com>
2022-09-05 09:47:14 +0800

    Add files via upload

---
+++ exam_1/App.java
@@ -0,0 +1,23 @@
+public class App {
+
+    public static void main(String[] args){
+        Student me = new Student();
+    
+        me.Surname = "Inovejas";
+        me.FirstName = "Kian Rotzen Genesis";
+        me.MiddleInitial = "E";
+        me.DateOfBirth = 01/17/2002; 
+        me.StudentNumber = 2022102360; 
+        me.StudentEmailAddress = "inovejaske@students.national-u.edu.ph";
+        me.iAmAwesome = true;
+        me.SayMyStudentNumber();
+        me.SayMyEmailAddress();
+        me.AmiAwesome(); 
+    }    
+
+}
+
+     
+
+
+
---
+++ exam_1/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+    
+String Surname;
+String FirstName;
+String MiddleInitial;
+int DateOfBirth; 
+int StudentNumber;
+String StudentEmailAddress;
+Boolean iAmAwesome;
+
+
+
+
+
+public void SayMyStudentNumber(){
+
+    System.out.println("My Student Number is " + StudentNumber); 
+}
+
+public void SayMyEmailAddress(){
+
+    System.out.println("My Email Address is " + StudentEmailAddress);
+}
+
+public void AmiAwesome(){
+
+    System.out.println(iAmAwesome);
+
+
+} 
+}
